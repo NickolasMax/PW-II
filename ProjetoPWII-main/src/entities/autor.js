0672dbd90936057c751ea0a6ffca4f51f0692d0a
Nickolas Maximiano <nickolasmpse@gmail.com>
2025-05-22 22:20:30 -0300

    Add files via upload

---
+++ ProjetoPWII-main/src/entities/autor.js
@@ -0,0 +1,15 @@
+import { EntitySchema } from "typeorm";
+
+const autor = new EntitySchema({
+    name: "Autor",
+    tableName:"autor",
+    columns: {
+        codigo_autor: {primary: true, type: "int", generated: "increment"},
+        nome_autor: {type: "varchar", length: 45, nullable: false},
+        nasc_autor: {type: "date"},
+        nacionalidade: {type: "varchar", length: 45, nullable: false},
+         
+    }
+});
+
+export default autor
